8128fae5d194b4dd7000dba50c7cf1362bb52f04
your-github-username <your-email@example.com>
2026-06-09 10:14:06 +0500

    feat: implement BFS and A* algorithms with Strategy pattern

--- src/solver.py
+++ src/solver.py
@@ -1,50 +1,97 @@
 # src/solver.py
+import heapq
 
 class Solver:
-    def __init__(self, maze):
+    def __init__(self, maze, algorithm="dfs"):
         self.maze = maze
+        self.algorithm = algorithm.lower()
 
     def solve(self):
-        # Using a Stack for DFS. Holds tuples of (current_cell, path_taken_so_far)
+        if self.algorithm == "bfs":
+            return self._solve_bfs()
+        elif self.algorithm == "astar":
+            return self._solve_astar()
+        else:
+            return self._solve_dfs() # Default to DFS
+
+    def _get_valid_neighbors(self, cell):
+        """Helper method to clean up our algorithms"""
+        neighbors = []
+        r, c = cell.row, cell.col
+        
+        if not cell.north_wall and r > 0:
+            neighbors.append(self.maze.grid[r - 1][c])
+        if not cell.south_wall and r < self.maze.rows - 1:
+            neighbors.append(self.maze.grid[r + 1][c])
+        if not cell.east_wall and c < self.maze.cols - 1:
+            neighbors.append(self.maze.grid[r][c + 1])
+        if not cell.west_wall and c > 0:
+            neighbors.append(self.maze.grid[r][c - 1])
+            
+        return neighbors
+
+    def _solve_dfs(self):
         stack = [(self.maze.start, [(self.maze.start.row, self.maze.start.col)])]
         visited = set()
 
         while stack:
-            # Notice we use .pop() here! This makes it a Stack (Last-In, First-Out)
-            # This is what makes it a DFS instead of a BFS.
-            current_cell, path = stack.pop()
+            current_cell, path = stack.pop() # Pop from end (Stack)
 
-            # If we reached the end, return the path!
             if current_cell == self.maze.end:
                 return path
 
             if current_cell in visited:
                 continue
-                
             visited.add(current_cell)
 
-            r = current_cell.row
-            c = current_cell.col
+            for neighbor in self._get_valid_neighbors(current_cell):
+                stack.append((neighbor, path + [(neighbor.row, neighbor.col)]))
+        return []
 
-            # Check North
-            if not current_cell.north_wall and r > 0:
-                neighbor = self.maze.grid[r - 1][c]
-                stack.append((neighbor, path + [(neighbor.row, neighbor.col)]))
-                
-            # Check South
-            if not current_cell.south_wall and r < self.maze.rows - 1:
-                neighbor = self.maze.grid[r + 1][c]
-                stack.append((neighbor, path + [(neighbor.row, neighbor.col)]))
-                
-            # Check East
-            if not current_cell.east_wall and c < self.maze.cols - 1:
-                neighbor = self.maze.grid[r][c + 1]
-                stack.append((neighbor, path + [(neighbor.row, neighbor.col)]))
-                
-            # Check West
-            if not current_cell.west_wall and c > 0:
-                neighbor = self.maze.grid[r][c - 1]
-                stack.append((neighbor, path + [(neighbor.row, neighbor.col)]))
+    def _solve_bfs(self):
+        queue = [(self.maze.start, [(self.maze.start.row, self.maze.start.col)])]
+        visited = set()
 
-        # If no path is found
+        while queue:
+            current_cell, path = queue.pop(0) # Pop from front (Queue)
+
+            if current_cell == self.maze.end:
+                return path
+
+            if current_cell in visited:
+                continue
+            visited.add(current_cell)
+
+            for neighbor in self._get_valid_neighbors(current_cell):
+                queue.append((neighbor, path + [(neighbor.row, neighbor.col)]))
+        return []
+
+    def _solve_astar(self):
+        # A* uses a priority queue based on: f_score = distance_traveled + estimated_distance_to_end
+        def heuristic(cell):
+            # Manhattan distance (rows away + cols away)
+            return abs(cell.row - self.maze.end.row) + abs(cell.col - self.maze.end.col)
+
+        count = 0 # Tie-breaker for the priority queue
+        # Queue stores: (f_score, count, current_cell, path)
+        queue = [(0, count, self.maze.start, [(self.maze.start.row, self.maze.start.col)])]
+        visited = set()
+
+        while queue:
+            _, _, current_cell, path = heapq.heappop(queue)
+
+            if current_cell == self.maze.end:
+                return path
+
+            if current_cell in visited:
+                continue
+            visited.add(current_cell)
+
+            for neighbor in self._get_valid_neighbors(current_cell):
+                g_score = len(path) # Distance traveled so far
+                h_score = heuristic(neighbor) # Guess to the end
+                f_score = g_score + h_score
+                
+                count += 1
+                heapq.heappush(queue, (f_score, count, neighbor, path + [(neighbor.row, neighbor.col)]))
         return []
